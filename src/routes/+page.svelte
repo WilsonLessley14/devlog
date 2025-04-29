a7cbc7f342fb386acb61af1b9cbb80e0b0935184
Wilson Lessley <wilsonlessley14@gmail.com>
2025-04-29 10:26:58 -0400

    feat: homepage copy w/ linkedin and github links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to The Cave!</h1>
+<p>My name is Wilson Lessley. I'm a software engineer and a fan of all things tech.</p>
+<p>
+	You can find me on <a href="https://github.com/WilsonLessley14" target="_blank">GitHub</a> and
+	<a href="https://linkedin.com/in/wilsonlessley" target="_blank">LinkedIn</a>.
+</p>
+<p>
+	I'm actively studying Graphics Engineering with the short goal of transitioning my career to
+	engine developement, and a long term dream of working on Chemical and Physical simulation engines
+	for research.
+</p>
